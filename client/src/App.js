d1e562e30060de776288ae372d9360f5d2c74c13
loun <lounas1994@gmail.com>
2022-11-13 12:16:15 +0100

    feat(client): add Routes for components in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Result from "./pages/Result";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/result" component={Result} />
+        <Route path="/:productId" component={Product} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
