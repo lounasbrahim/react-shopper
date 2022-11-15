d348065229f138f3ca695525274cfbed5af1902d
loun <lounas1994@gmail.com>
2022-11-15 14:22:08 +0100

    feat(server): add product route in server

--- server/routes.js
+++ server/routes.js
@@ -4,9 +4,18 @@ const products = require("./products.json");
 module.exports = function getRoutes() {
   const router = express.Router();
 
-  router.get("/products", (req, res) => {
-    res.status(200).json({ products });
-  });
+  router.get("/products", getProducts);
+  router.get("/products/:productId", getProduct);
 
   return router;
 };
+
+function getProducts(req, res) {
+  return res.status(200).json({ products });
+}
+
+function getProduct(req, res) {
+  const { productId } = req.params;
+  const product = products.find((product) => product.id === productId);
+  return res.status(200).json({ product });
+}
