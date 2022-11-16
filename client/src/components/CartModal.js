f5cdcd00c33d4d23792cf64076d6e55bf88378c0
loun <lounas1994@gmail.com>
2022-11-16 20:40:56 +0100

    feat(server): add Error message when clicking on checkout when there is no item in the cart

--- client/src/components/CartModal.js
+++ client/src/components/CartModal.js
@@ -45,6 +45,7 @@ export default function CartModal({ isCartModalOpen: isOpen, toggleModal }) {
                 onClick={handleCheckout}
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 style={{ marginRight: 4 }}
+                disabled={!cartCount}
               >
                 Checkout Now
               </button>
--- client/src/components/CheckoutCart.js
+++ client/src/components/CheckoutCart.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import toast from 'react-hot-toast'
 import { useShoppingCart } from 'use-shopping-cart'
 import useCheckout from '../utils/useCheckout'
 
 export default function CheckoutCart() {
   const { cartCount } = useShoppingCart()
   const handleCheckout = useCheckout()
+
+  function handleCheckoutCart() {
+    if (cartCount) return handleCheckout()
+    return toast.error('There is no Items in your Cart')
+  }
   return (
     <button
-      onClick={handleCheckout}
+      onClick={handleCheckoutCart}
       className="inline-flex items-center bg-green-700 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-white mt-4 md:mt-0"
-      disabled={!cartCount}
     >
       Go To Checkout
       <svg
