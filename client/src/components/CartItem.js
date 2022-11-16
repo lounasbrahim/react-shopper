dfe1bf6b365ab9f2e7ea26a1ea9d94f7fddd468d
loun <lounas1994@gmail.com>
2022-11-16 12:58:35 +0100

    feat(client): handle and display cart items

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useShoppingCart } from "use-shopping-cart";
 
-export default function CartItem() {
+import formatProductPrice from "../utils/formatProductPrice";
+
+export default function CartItem({ cartItem }) {
+  const { setItemQuantity } = useShoppingCart();
+
+  const price = formatProductPrice(cartItem);
+
+  function handleCartItemQuantity(e) {
+    setItemQuantity(cartItem.sku, e.target.value);
+  }
+
   return (
     <div className="flex w-full">
       <div className="flex items-center px-4 py-3 hover:bg-gray-100 -mx-4 w-full justify-between">
         <div className="flex">
           <img
             className="h-16 w-16 rounded-full object-cover mx-1"
-            src="https://dummyimage.com/200x200"
-            alt="avatar"
+            src={cartItem.image}
+            alt={cartItem.name}
           />
           <p className="text-gray-600 text-lg mx-2">
-            <span className="font-bold">Name</span> <br />
-            Price x Quantity
+            <span className="font-bold">{cartItem.name}</span> <br />
+            {price} x {cartItem.quantity}
           </p>
         </div>
         <div>
           <input
+            onChange={handleCartItemQuantity}
             style={{ width: 50 }}
             className="border-solid border-2"
             type="number"
-            value={0}
+            value={cartItem.quantity}
             min={0}
           />
         </div>
--- client/src/components/CartModal.js
+++ client/src/components/CartModal.js
@@ -2,11 +2,14 @@
 import React from "react";
 import Modal from "react-modal";
 import { useShoppingCart } from "use-shopping-cart";
+import CartItem from "./CartItem";
 
 Modal.setAppElement("#root");
 
 export default function CartModal({ isCartModalOpen: isOpen, toggleModal }) {
-  const { cartCount, formattedTotalPrice } = useShoppingCart();
+  const { cartCount, formattedTotalPrice, cartDetails } = useShoppingCart();
+
+  const cartItems = Object.keys(cartDetails).map((key) => cartDetails[key]);
 
   return (
     <Modal
@@ -24,6 +27,15 @@ export default function CartModal({ isCartModalOpen: isOpen, toggleModal }) {
               </div>
             </div>
             <hr />
+            {cartCount > 0 ? (
+              cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} cartItem={cartItem} />
+              ))
+            ) : (
+              <div className="text-gray-500 text-sm">
+                There is no Item in your cart
+              </div>
+            )}
             <div className="ml-auto mt-4">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
