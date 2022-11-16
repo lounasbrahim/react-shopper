cce27213d89a314415fb2627f3bd3359a1dda5c1
loun <lounas1994@gmail.com>
2022-11-16 18:16:23 +0100

    feat(server): add checkout-sessions endpoint to server

--- server/routes.js
+++ server/routes.js
@@ -1,30 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const products = require("./products.json");
+require('dotenv').config()
+const express = require('express')
+const products = require('./products.json')
 
 module.exports = function getRoutes() {
-  const router = express.Router();
+  const router = express.Router()
 
-  router.get("/products", getProducts);
-  router.get("/products/:productId", getProduct);
-
-  return router;
-};
+  router.get('/products', getProducts)
+  router.get('/products/:productId', getProduct)
+  router.post('/checkout-sessions', createCheckoutSession)
+  return router
+}
 
 function getProducts(req, res) {
-  return res.status(200).json({ products });
+  return res.status(200).json({ products })
 }
 
 function getProduct(req, res) {
   try {
-    const { productId } = req.params;
-    const product = products.find((product) => product.id === productId);
+    const { productId } = req.params
+    const product = products.find(product => product.id === productId)
 
     if (!product) {
-      throw Error(`there is no Product for id : ${productId}`);
+      throw Error(`there is no Product for id : ${productId}`)
     }
-    return res.status(200).json({ product });
+    return res.status(200).json({ product })
   } catch (error) {
-    return res.status(404).json({ statusCode: 404, messsage: error.message });
+    return res.status(404).json({ statusCode: 404, messsage: error.message })
   }
 }
+function createCheckoutSession() {}
