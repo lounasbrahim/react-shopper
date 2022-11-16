205d5ac0521bfd4b6bac4617411a312a5e92dd3e
loun <lounas1994@gmail.com>
2022-11-16 10:15:52 +0100

    feat(client): hanlde remove item from cart

--- client/src/components/RemoveFromCart.js
+++ client/src/components/RemoveFromCart.js
@@ -1,8 +1,28 @@
 import React from "react";
+import toast from "react-hot-toast";
+import { useShoppingCart } from "use-shopping-cart";
 
-export default function RemoveFromCart() {
+export default function RemoveFromCart({ product }) {
+  const { removeItem, cartCount, cartDetails, totalPrice } = useShoppingCart();
+
+  function handleRemoveItem() {
+    removeItem(product.sku);
+    return isItemInCart(product.sku)
+      ? toast.success(`${product.name} is removed from your Cart`)
+      : toast.error(`The is no ${product.name} in your Cart`);
+  }
+
+  const isItemInCart = (productSku) => {
+    return cartDetails[productSku] ? true : false;
+  };
+
+  console.log({ cartCount, cartDetails, totalPrice });
   return (
-    <button className="flex ml-2 text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+    <button
+      onClick={handleRemoveItem}
+      className="flex ml-2 text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
+      disabled={!cartCount}
+    >
       Remove
     </button>
   );
